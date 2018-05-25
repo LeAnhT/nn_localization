e1586ac1c8b4a549047cf1c58f406518953caaab
LeAnhT <xtuleanh@yahoo.de>
2018-05-25 18:52:10 +0200

    changes on plot_helper

--- plot_helper.py
+++ plot_helper.py
@@ -4,16 +4,18 @@ import numpy as np
 def plot_ecdf(values,x_label,y_label):
     x = np.sort(values)
     y = np.arange(0,len(x))/len(x)
-    plt.plot(x, y, marker='.', linestyle='none')
+    plt.plot(x, y,'grey', marker='.', linestyle='none')
     plt.xlabel(x_label)
     plt.ylabel(y_label)
+    plt.xticks(np.arange(0,1,0.1))
     #plt.title(title)
     plt.show()
 
 def simple_plot(values,x_label,y_label):
-    plt.plot(np.arange(0,len(values)),values)
+    plt.plot(np.arange(0,len(values)),values,'grey')
     plt.xlabel(x_label)
     plt.ylabel(y_label)
+    plt.set_cmap('Greys')
     plt.show()
 
 def plot_grid_contour(x_size,y_size,step,grid_errors,x_label,y_label):
@@ -21,7 +23,12 @@ def plot_grid_contour(x_size,y_size,step,grid_errors,x_label,y_label):
     Y = np.arange(0, y_size + step, step)
     grid = np.array(grid_errors).reshape(len(X),len(Y))
 
-    plt.contour(X,Y,grid)
+    CS = plt.contour(X,Y,grid)
+    for i in range(1,len(CS.collections)+1):
+        CS.collections[i-1].set_label('line'+str(i))
+    #plt.legend(loc='best')
+    plt.clabel(CS,inline=1,fontsize=10)
     plt.xlabel(x_label)
     plt.ylabel(y_label)
+    plt.set_cmap('Greys')
     plt.show()
